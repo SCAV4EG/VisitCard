a7b06188b6d19ea2fc7438882ec57ed43914c5bd
SCAV4EG <147843239+SCAV4EG@users.noreply.github.com>
2025-02-24 23:27:19 +0300

    Delete send_messages.php

--- send_messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Проверяем, было ли отправлено сообщение
-if (isset($_POST['message'])) {
-    $message = htmlspecialchars($_POST['message']); // Защита от XSS
-
-    // Записываем сообщение в файл
-    file_put_contents('chat.txt', $message . "\n", FILE_APPEND);
-}
-?>
